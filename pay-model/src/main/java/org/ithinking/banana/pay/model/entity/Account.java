690a23ab7708ab914f7ac8dcb2c7a171b62efab5
vajava@126.com <vajava@126.com>
2016-05-07 14:27:20 +0800

    完善

--- pay-model/src/main/java/org/ithinking/banana/pay/model/entity/Account.java
+++ pay-model/src/main/java/org/ithinking/banana/pay/model/entity/Account.java
@@ -1,16 +1,25 @@
 package org.ithinking.banana.pay.model.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 /**
  * 账号实体类
  *
  * @author agan
  * @date 2016-05-07
  */
+@Entity
+@Table(name = "pay_account")
 public class Account {
     private Long accountId;
     private Long ownerId;
     private Long ammount;
 
+    @Id
+    @Column(name = "account_id", nullable = false)
     public Long getAccountId() {
         return accountId;
     }
@@ -19,6 +28,7 @@ public class Account {
         this.accountId = accountId;
     }
 
+    @Column(name = "owner_id", nullable = false)
     public Long getOwnerId() {
         return ownerId;
     }
@@ -27,6 +37,7 @@ public class Account {
         this.ownerId = ownerId;
     }
 
+    @Column(name = "ammount", nullable = false)
     public Long getAmmount() {
         return ammount;
     }
