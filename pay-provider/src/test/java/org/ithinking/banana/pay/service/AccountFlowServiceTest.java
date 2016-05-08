c3b89017d91cf36f2694e193811a357ebf3df2c4
vajava@126.com <vajava@126.com>
2016-05-09 01:08:06 +0800

    完善

--- pay-provider/src/test/java/org/ithinking/banana/pay/service/AccountFlowServiceTest.java
+++ pay-provider/src/test/java/org/ithinking/banana/pay/service/AccountFlowServiceTest.java
@@ -1,9 +1,17 @@
 package org.ithinking.banana.pay.service;
 
+import org.ithinking.banana.comm.IdWorker;
 import org.ithinking.banana.pay.Application;
+import org.ithinking.banana.pay.model.entity.AccountFlow;
+import org.ithinking.banana.pay.service.impl.AccountFlowServiceImpl;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.annotation.Resource;
+import java.util.Date;
+import java.util.Random;
 
 /**
  * ${TITLE}
@@ -15,4 +23,35 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @SpringApplicationConfiguration(classes = Application.class)
 public class AccountFlowServiceTest {
 
+    @Resource
+    private AccountFlowService accountFlowService;
+    @Resource
+    private IdWorker idWorker;
+
+    private Long flowById = 1111111L;
+    private Long ownerId = 1111111L;
+    private Long accountId = 1111111L;
+    private String fromAccountId = "222222";
+
+    @Test
+    public void testGetAccountFlowById() {
+        AccountFlow accountFlow = accountFlowService.getAccountFlowById(flowById);
+        System.out.println(accountFlow);
+    }
+
+    @Test
+    public void testAddAccountFlow() {
+        AccountFlow accountFlow = new AccountFlow();
+
+        accountFlow.setFlowId(idWorker.nextId());
+        accountFlow.setAccountId(accountId);
+        accountFlow.setAmount(new Random().nextLong());
+        accountFlow.setBalance(new Random().nextLong());
+        accountFlow.setFromAccountId(fromAccountId);
+        accountFlow.setRemark("测试");
+        accountFlow.setCreateTime(new Date());
+
+        accountFlowService.addAccountFlow(accountFlow);
+        System.out.println("addAccountFlow OK!");
+    }
 }
