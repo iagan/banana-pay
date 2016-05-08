f1ee92492326af27cb9c8308af5e6b18086167b4
vajava@126.com <vajava@126.com>
2016-05-09 00:59:38 +0800

    完善

--- pay-mapper/src/main/java/org/ithinking/banana/pay/mapper/AccountFlowMapper.java
+++ pay-mapper/src/main/java/org/ithinking/banana/pay/mapper/AccountFlowMapper.java
@@ -1,5 +1,7 @@
 package org.ithinking.banana.pay.mapper;
 
+import org.ithinking.banana.pay.model.entity.AccountFlow;
+
 /**
  * 账号变更流水
  *
@@ -7,4 +9,19 @@ package org.ithinking.banana.pay.mapper;
  * @date 2016-05-09
  */
 public interface AccountFlowMapper {
+
+    /**
+     * 新增账号流水
+     *
+     * @param accountFlow
+     */
+    void addAccountFlow(AccountFlow accountFlow);
+
+    /**
+     * 获取账号流水
+     *
+     * @param flowId
+     * @return
+     */
+    AccountFlow getAccountFlowById(Long flowId);
 }
--- pay-service/src/main/java/org/ithinking/banana/pay/service/impl/AccountFlowServiceImpl.java
+++ pay-service/src/main/java/org/ithinking/banana/pay/service/impl/AccountFlowServiceImpl.java
@@ -1,7 +1,11 @@
 package org.ithinking.banana.pay.service.impl;
 
+import org.ithinking.banana.pay.mapper.AccountFlowMapper;
 import org.ithinking.banana.pay.model.entity.AccountFlow;
 import org.ithinking.banana.pay.service.AccountFlowService;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
 
 /**
  * 账单流水明细
@@ -9,15 +13,20 @@ import org.ithinking.banana.pay.service.AccountFlowService;
  * @author agan
  * @date 2016-05-09
  */
-public class AccountFlowServiceImpl implements AccountFlowService{
+@Service("accountFlowService")
+public class AccountFlowServiceImpl implements AccountFlowService {
+
+    @Resource
+    private AccountFlowMapper accountFlowMapper;
 
     @Override
     public void addAccountFlow(AccountFlow accountFlow) {
-
+        accountFlowMapper.addAccountFlow(accountFlow);
     }
 
     @Override
     public AccountFlow getAccountFlowById(Long flowId) {
-        return null;
+        AccountFlow accountFlow = accountFlowMapper.getAccountFlowById(flowId);
+        return accountFlow;
     }
 }
